ede1957f6e00071716b7f4f753d93bc8ad3133bd
MayraGonzalezN <mayragonzalezdd@gmail.com>
2024-10-18 15:03:12 -0300

    Agregada clase Carrito

---
+++ Dominio/Carrito.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Carrito
+    {
+        public List<Producto> Productos { get; set; }
+        //public Cliente Cliente { get; set; }
+    }
+}
